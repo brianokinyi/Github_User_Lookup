6c3f21139f18df6437e2e53d7b097039507a1f16
brianokinyi <brianokinyi.bo@gmail.com>
2018-03-03 09:08:17 +0300

    Added more details to details

--- src/app/details.ts
+++ src/app/details.ts
@@ -4,5 +4,11 @@ export class Details {
   company:string;
   url:string;
   avatar_url:string;
+  location: string;
+  email: string;
   bio:string;
+  followers: number;
+  following: number;
+  public_repos: number;
+  created_at: string;
 }
